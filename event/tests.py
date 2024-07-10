0adc2d80b1b75efddd05bb6a5b1bf6b2a5fcfa18
kadimasum <kadimasam@gmail.com>
2024-07-10 14:24:58 +0300

    Add tests

--- event/tests.py
+++ event/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class EventPageTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index_page(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'event/index.html')
+        self.assertContains(response, 'Kubernetes Learning Series')
+        self.assertContains(response, 'Join us for an exciting learning series on Kubernetes')
+        self.assertContains(response, 'Date: July 27, 2024')
